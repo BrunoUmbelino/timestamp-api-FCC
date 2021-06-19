5f9e96b117e0f2492e7b2eb85933c7bcf1ed1c2d
Bruno <bruno.umbelino0@gmail.com>
2021-06-19 20:01:51 -0300

    implementing functionality to receive a long Date and improving variable names

--- src/Controller.js
+++ src/Controller.js
@@ -1,11 +1,17 @@
 const date = (request, response) => {
   const param = request.params.date;
 
-  const dateValidation = /(\d\d\d\d)-(\d\d)-(\d\d)/g.test(param);
-  const unixValidation = /(^\d+$)/g.test(param);
+  const shortDateValidation = /^(\d{4})-(\d{2})-(\d{2})$/g.test(param);
+  const longDateValidation = /^(\d{2}) (\w+) (\d{4})$/g.test(param);
+  const unixDateValidation = /^(\d+)$/g.test(param);
   const undefinedParam = param === undefined;
 
-  if (dateValidation || unixValidation || undefinedParam) {
+  if (
+    shortDateValidation ||
+    longDateValidation ||
+    unixDateValidation ||
+    undefinedParam
+  ) {
     if (undefinedParam) {
       const date = new Date();
       const utc = date.toUTCString();
@@ -14,15 +20,15 @@ const date = (request, response) => {
 
       response.json({ dateStr, utc, unix });
     }
-    if (dateValidation) {
-      const date = new Date();
+    if (shortDateValidation || longDateValidation) {
+      const date = new Date(param);
       const utc = date.toUTCString();
       const dateStr = date.toString();
       const unix = Date.parse(param);
 
       response.json({ dateStr, utc, unix });
     }
-    if (unixValidation) {
+    if (unixDateValidation) {
       const unix = parseInt(param);
       const date = new Date(unix);
       const utc = date.toUTCString();
